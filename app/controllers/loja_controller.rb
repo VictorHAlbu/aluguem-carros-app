5285f24896b432cf264229725b1a07488616f3ff
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-04-17 21:49:10 -0300

    Estilizando a area de aluguel

--- app/controllers/loja_controller.rb
+++ app/controllers/loja_controller.rb
@@ -1,11 +1,17 @@
 class LojaController < UsuariosController
-  before_action :set_veiculos, only: :aluguel
+  before_action :set_veiculos, only: [:aluguel, :alugar]
 
   def index
   end
 
   def aluguel
     
+  end
+
+  def alugar
+    if params[:dias].present?
+      dias = params[:dias]
+    @valor_total = dias * @veiculo.valor
   end
 
   def set_veiculos
