ceb5f7403e9f6461a8af165c7cf23196faf45b99
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-01-27 14:30:31 -0300

    ajustando a ação alugar

--- app/controllers/loja_controller.rb
+++ app/controllers/loja_controller.rb
@@ -12,31 +12,31 @@ class LojaController < UsuariosController
   end
 
   def alugar
-    if params[:cardNumber].present? && 
-      params[:cardName].present? && 
-      params[:expitionDate].present? && 
-      params[:cvv].present?
-      
-      if params[:dias].present?
+    debugger
+      if params[:dias].present? && params[:token].present?
         dias = params[:dias].to_i
+        token_pagamento = params[:token]
         @valor_total = dias * @veiculo.valor
+        if cookies[:usuario].present?  
           reserva = Reserva.new(reservado_de: Time.now, reservado_ate: Time.now + dias.days, valor_alugado: @valor_total, 
           status: Reserva::STATUS[:aguardando], veiculo_id: @veiculo.id, usuario_id: cookies[:usuario])
           if reserva.save!
-            redirect_to "/confirmacao_pagamento/#{reserva.id}"
+            redirect_to "/confirmacao_pagamento/#{reserva.id}?token_pagamento=#{token_pagamento}"
             return
           end
           else
             redirect_to "/login"
           return
         end
-      end
-    
-    redirect_to "/loja/locacao/#{@veiculo.id}", notice: "Houve um erro ao ir a tela de confirmação"
+      end 
+      redirect_to "/loja/locacao/#{@veiculo.id}", notice: 'Houve um erro ao ir a tela de confirmação'
   end
 
   def confirmacao_pagamento
+    if params[:token_pagamento].present? 
+      @token = params[:token_pagamento]
       @reserva = Reserva.find(params[:reserva_id])
+    end
   end
 
   private
